80e0d50ddba641462ad4285fc6aea4c8204a4c5c
Sahil Kasare <cdsahilkasare@gmail.com>
2026-02-04 14:57:00 +0530

    Change Supabase client URL and key
    Updated Supabase URL and key for the client.

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://nktttdmpodskbjvaobox.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rdHR0ZG1wb2Rza2JqdmFvYm94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyNTc4OTIsImV4cCI6MjA1MTgzMzg5Mn0.iH-Z6h6k747u07-xGfulvVpuOAv0KrWV1frMTrfaIDY';
+const supabaseUrl = 'https://vewotcnnzjydwwpcznzk.supabase.co';
+const supabaseKey = 'sb_publishable_UXavzK6Ey0O0C3ZxHW6Hlw_RwrjSmBt';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
